b0e96c6ea767ac084ec059f0be9b665255a597d8
alyssonflp <alysson.flp@hotmail.com>
2026-02-22 23:06:56 -0300

    Update info_sistema.js

--- js/info_sistema.js
+++ js/info_sistema.js
@@ -1,58 +1,57 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const desktop = document.getElementById("desktop-3d");
+/**
+ * Módulo de Informações do Sistema
+ * Captura IP, Localização, Navegador e OS
+ */
+export async function initSystemInfo() {
+    // Detectar Navegador
+    let browser = "UNKNOWN";
+    const ua = navigator.userAgent;
+    if (ua.indexOf("Firefox") > -1) browser = "FIREFOX";
+    else if (ua.indexOf("Chrome") > -1) browser = "CHROME";
+    else if (ua.indexOf("Safari") > -1) browser = "SAFARI";
+    else if (ua.indexOf("Edge") > -1) browser = "EDGE";
 
-    const infoFooter = document.createElement("div");
-    infoFooter.id = "info-footer";
-    desktop.appendChild(infoFooter);
+    // Detectar Sistema Operacional
+    let os = "OS_UNKNOWN";
+    const platform = navigator.platform.toLowerCase();
+    if (platform.indexOf("win") !== -1) os = "WINDOWS";
+    else if (platform.indexOf("mac") !== -1) os = "MACOS";
+    else if (platform.indexOf("linux") !== -1) os = "LINUX";
+    else if (/android|iphone|ipad|ipod/.test(ua.toLowerCase())) os = "MOBILE";
 
-    async function getSystemData() {
-        // Detectar Navegador
-        let browser = "UNKNOWN";
-        const ua = navigator.userAgent;
-        if (ua.indexOf("Firefox") > -1) browser = "FIREFOX";
-        else if (ua.indexOf("Chrome") > -1) browser = "GOOGLE_CHROME";
-        else if (ua.indexOf("Safari") > -1) browser = "SAFARI";
-        else if (ua.indexOf("Edge") > -1) browser = "MS_EDGE";
+    const footer = document.getElementById("info-footer");
+    if (!footer) return;
 
-        // Detectar Sistema Operacional
-        let os = "OS_UNKNOWN";
-        const platform = navigator.platform.toLowerCase();
-        if (platform.indexOf("win") !== -1) os = "WINDOWS_OS";
-        else if (platform.indexOf("mac") !== -1) os = "MACOS";
-        else if (platform.indexOf("linux") !== -1) os = "LINUX_SYS";
-        else if (/android|iphone|ipad|ipod/.test(ua.toLowerCase())) os = "MOBILE_DEVICE";
+    try {
+        // Buscando IP e Localização
+        const response = await fetch('https://ipapi.co/json/');
+        if (!response.ok) throw new Error();
+        const data = await response.json();
 
-        try {
-            const response = await fetch('https://ipapi.co/json/');
-            const data = await response.json();
-
-            infoFooter.innerHTML = `
-                <div class="footer-group">
-                    <div class="info-item">
-                        <span class="label">NODE_IP:</span> 
-                        <span class="value">${data.ip}</span>
-                    </div>
-                    <div class="info-item hide-mobile">
-                        <span class="label">BROWSER:</span> 
-                        <span class="value">${browser}</span>
-                    </div>
+        footer.innerHTML = `
+            <div class="footer-group">
+                <div class="info-item">
+                    <span class="label">NODE_IP:</span> 
+                    <span class="value">${data.ip}</span>
                 </div>
-                
-                <div class="footer-group">
-                    <div class="info-item hide-mobile">
-                        <span class="label">OS:</span> 
-                        <span class="value">${os}</span>
-                    </div>
-                    <div class="info-item">
-                        <span class="label">LOC:</span> 
-                        <span class="value">${data.city}, ${data.region_code}</span>
-                    </div>
+                <div class="info-item hide-mobile">
+                    <span class="label">BROWSER:</span> 
+                    <span class="value">${browser}</span>
                 </div>
-            `;
-        } catch (error) {
-            infoFooter.innerHTML = `<span>[ SYSTEM_CORE_RECOVERY_MODE ]</span>`;
-        }
+            </div>
+            
+            <div class="footer-group">
+                <div class="info-item hide-mobile">
+                    <span class="label">SYS_OS:</span> 
+                    <span class="value">${os}</span>
+                </div>
+                <div class="info-item">
+                    <span class="label">LOCATION:</span> 
+                    <span class="value">${data.city}, ${data.region_code}</span>
+                </div>
+            </div>
+        `;
+    } catch (error) {
+        footer.innerHTML = `<div style="width:100%; text-align:center; opacity:0.5;">[ SYSTEM_SECURE_MODE_ACTIVE ]</div>`;
     }
-
-    getSystemData();
-});
+}
