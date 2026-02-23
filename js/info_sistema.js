4d1f403ea8c8b8dff333290399152723a67c580c
alyssonflp <alysson.flp@hotmail.com>
2026-02-23 02:01:03 -0300

    Update info_sistema.js

--- js/info_sistema.js
+++ js/info_sistema.js
@@ -2,14 +2,14 @@ export async function initSystemInfo() {
 
     const footer = document.getElementById("info-footer");
 
-    if (!footer) {
-        console.log("Footer não encontrado");
-        return;
-    }
+    if (!footer) return;
+
+    const ua = navigator.userAgent;
 
     footer.innerHTML = `
-        <div style="color:lime;font-size:16px;">
-            SISTEMA ATIVO
+        <div style="color:lime;font-size:14px;">
+            USER AGENT:<br>
+            ${ua}
         </div>
     `;
 }
