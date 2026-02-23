c379ff9cd8823f9fb8fdc951c005ab76ee2d1212
alyssonflp <alysson.flp@hotmail.com>
2026-02-23 10:55:04 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,7 +21,8 @@ export async function startOS() {
 
     const interfaceModule = await safeImport("./interface_3d.js");
     const infoModule = await safeImport("./info_sistema.js");
-    const terminalModule = await safeImport("./terminal.js"); // Novo módulo
+    const terminalModule = await safeImport("./terminal.js");
+    const dockModule = await safeImport("./dock.js"); // Importando o Dock
 
     // Inicializa Interface 3D
     try {
@@ -33,10 +34,15 @@ export async function startOS() {
         await infoModule?.initSystemInfo?.();
     } catch (e) { console.warn("Erro ao iniciar Info Sistema"); }
 
-    // Inicializa o Terminal (Hello World -> Prompt)
+    // Inicializa o Terminal (Direto para o Prompt)
     try {
         await terminalModule?.initTerminal?.();
     } catch (e) { console.warn("Erro ao iniciar Terminal"); }
+
+    // Inicializa o Dock de Ícones
+    try {
+        await dockModule?.initDock?.();
+    } catch (e) { console.warn("Erro ao iniciar Dock"); }
 }
 
 window.startOS = startOS;
